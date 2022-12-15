0827cbe150770c4d20757d4858437e1cb0342044
albertuscalvin <93346386+albertuscalvin@users.noreply.github.com>
2022-12-15 16:13:50 +0700

    Add files via upload

---
+++ 1_D_71210733.py
@@ -0,0 +1,24 @@
+import turtle
+sc= turtle.Screen()
+ac = turtle.Turtle()
+for i in range(4):
+    ac.
+    ac.color("violet")
+    ac.forward(300)
+    ac.left(90)
+
+    ac.pensize(30)
+    ac.color("white")
+    ac.circle(95, -180)
+
+    ac.pensize(30)
+    ac.color("white")
+    ac.forward(130)
+    ac.right(45)
+    ac.forward(50)
+    ac.backward(150)
+
+    ac.right(90)
+    ac.forward(150)
+
+import turtle as ac
